1901918cbe08beaf94988838cdcf86f3e2c6ca7c
seaman248 <semen.bko@ya.ru>
2017-03-10 21:55:30 +0700

    move data files to data directory

--- parse.py
+++ parse.py
@@ -3,7 +3,7 @@ from bs4 import BeautifulSoup as BS
 import re
 import requests
 
-with open('./genes.json', 'r') as input:
+with open('./data/genes.json', 'r') as input:
     genes = json.load(input)
 
 base_url = 'https://www.vectorbase.org/Anopheles_stephensi/Component/Gene/Compara_Alignments/alignments?align=9160;db=core;g='
@@ -21,5 +21,5 @@ for gene in genes[1:10]:
             })
 
 
-with open('./orths.json', 'w') as output:
+with open('./data/orths.json', 'w') as output:
     json.dump(orths, output)
