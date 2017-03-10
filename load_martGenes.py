034f5167f86a29eaefeb99b4143382cc3747533b
seaman248 <semen.bko@ya.ru>
2017-03-10 19:53:16 +0700

    load genes of interest

---
+++ load_martGenes.py
@@ -0,0 +1,22 @@
+from biomart import BiomartServer
+import json
+
+server = BiomartServer('http://biomart.vectorbase.org/biomart')
+
+steph = server.datasets['astephensi_eg_gene']
+
+response = steph.search({
+    'attributes': [
+        'ensembl_gene_id'
+    ]
+})
+
+genes = []
+for line in response.iter_lines():
+    line = line.decode('utf-8')
+    genes.append(line)
+
+print(genes)
+
+with open('./genes.json', 'w') as file:
+    json.dump(genes, file)
