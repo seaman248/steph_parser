c428e7ac1f7b8ee19be91bbb3908dd223ee64d43
seaman248 <semen.bko@ya.ru>
2017-03-10 21:54:56 +0700

    remove timetest

--- parse.py
+++ parse.py
@@ -1,7 +1,6 @@
 import json
 from bs4 import BeautifulSoup as BS
 import re
-import datetime as dt
 import requests
 
 with open('./genes.json', 'r') as input:
@@ -9,12 +8,8 @@ with open('./genes.json', 'r') as input:
 
 base_url = 'https://www.vectorbase.org/Anopheles_stephensi/Component/Gene/Compara_Alignments/alignments?align=9160;db=core;g='
 
-ngenes = len(genes)
-
 orths = []
 
-last_time = dt.datetime.today().timestamp()
-diffs = []
 for gene in genes[1:10]:
     page = requests.get(base_url + gene)
     tds = BS(page.text, 'html.parser').findAll('td')
@@ -25,13 +20,6 @@ for gene in genes[1:10]:
                 'AsteI2': td.text
             })
 
-    new_time = dt.datetime.today().timestamp()
-    diffs.append(new_time - last_time)
-    last_time = new_time
-    if len(diffs) > 10:
-        diffs = diffs[-10:]
-    time_per_it = len(diffs) / sum(diffs)
-    print(ngenes / time_per_it / 60 / 60)
 
 with open('./orths.json', 'w') as output:
     json.dump(orths, output)
